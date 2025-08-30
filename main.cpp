f1a6dab8581592067e2608e7d4540327b93e7840
Torin Scholes <torinscholes@gmail.com>
2025-08-30 15:48:11 -0500

    Setup the physical and logical vulkan devices to be used for the project.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
+#include <optional>
 
 constexpr uint32_t WIDTH = 800;
 constexpr uint32_t HEIGHT = 600;
@@ -20,12 +22,15 @@ public:
 private:
     GLFWwindow* window;
     VkInstance vulkInstance;
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+    VkDevice device;
+    VkQueue graphicQueue;
 
     void initWindow() {
         glfwInit();
 
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // We don't need the openGL stuff
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // Important for swapchain shenanigans
 
         window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
     }
@@ -33,15 +38,118 @@ private:
     void initVulkan() {
         // Overview VkInstance(the Vulkan API Context) -> VkPhysicalDevice(a reference to a GPU) -> VkDevice(a reference to the GPU Driver)
         createInstance();
-        //VkPhysicalDevice vulkPhysDevice = ;
-        //VkDevice vulkDevice = ;
+        pickPhysicalDevice();
+        createLogicalDevice();
 
         // A swapchain is a series of buffers/images you can draw to then later display to the screen.
         // NOTE: swapchains are created for a given size if the screen is resized then you need to recreate the swapchain.
 
     }
 
+    void createLogicalDevice() {
+        QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+
+        // Specify the queues to be created
+        VkDeviceQueueCreateInfo queueCreateInfo{};
+        queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+        queueCreateInfo.queueCount = 1;
+        float queuePriority = 1.0f;
+        queueCreateInfo.pQueuePriorities = &queuePriority;
+
+        // Specify the device features to use
+        VkPhysicalDeviceFeatures deviceFeatures{};
+
+        // Create the logical device
+        VkDeviceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+        createInfo.pQueueCreateInfos = &queueCreateInfo;
+        createInfo.queueCreateInfoCount = 1;
+        createInfo.pEnabledFeatures = &deviceFeatures;
+
+        createInfo.enabledExtensionCount = 0;
+
+        if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
+            throw std::runtime_error("Failed to create logical device.");
+        }
+    }
+
+    struct QueueFamilyIndices {
+        std::optional<uint32_t> graphicsFamily;
+
+        bool isComplete() {
+            return graphicsFamily.has_value();
+        }
+    };
+
+    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
+        QueueFamilyIndices indices;
+
+        uint32_t queueFamilyCount = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+        std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+        int i = 0;
+        for (const auto& queueFamily : queueFamilies) {
+            if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+                indices.graphicsFamily = i;
+            }
+
+            if (indices.isComplete()) {
+                break;
+            }
+
+            i++;
+        }
+
+        return indices;
+    }
+
+    void pickPhysicalDevice() {
+        uint32_t deviceCount = 0;
+        vkEnumeratePhysicalDevices(vulkInstance, &deviceCount, nullptr);
+
+        if (deviceCount == 0) {
+            throw std::runtime_error("Failed to find any GPUs with Vulkan Support.");
+        }
+
+        std::vector<VkPhysicalDevice> devices(deviceCount);
+        vkEnumeratePhysicalDevices(vulkInstance, &deviceCount, devices.data());
+
+        for (const auto& device : devices) {
+            if (isDeviceSuitable(device)) {
+                physicalDevice = device;
+                break;
+            }
+        }
+
+        if (physicalDevice == VK_NULL_HANDLE) {
+            throw std::runtime_error("Failed to find GPU.");
+        }
+    }
+
+    bool isDeviceSuitable(VkPhysicalDevice device) {
+        //VkPhysicalDeviceProperties deviceProperties;
+        //vkGetPhysicalDeviceProperties(device, &deviceProperties);
+
+        //VkPhysicalDeviceFeatures deviceFeatures;
+        //vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+
+        QueueFamilyIndices indices = findQueueFamilies(device);
+
+
+        // TODO: give score to available GPUs
+        return indices.isComplete();
+    }
+
     void createInstance() {
+        // Setup Vulkan validation layers
+        const VkBool32 verboseValue = true;
+        const VkLayerSettingEXT layerSettings = {"VK_LAYER_KHRONOS_validation", "printf_verbose", VK_LAYER_SETTING_TYPE_BOOL32_EXT, 1, &verboseValue};
+        VkLayerSettingsCreateInfoEXT layerSettingsCreateInfo = { VK_STRUCTURE_TYPE_LAYER_SETTINGS_CREATE_INFO_EXT, nullptr, 1, &layerSettings };
+
         // Specify the application info for Vulkan
         VkApplicationInfo appInfo{};
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
@@ -58,9 +166,10 @@ private:
         uint32_t glfwExtensionCount = 0;
         const char** glfwExtensions;
         glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-        createInfo.enabledExtensionCount = glfwExtensionCount;
-        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledExtensionCount = glfwExtensionCount; // NOTE: these are ignored in modern implementation of vulkan
+        createInfo.ppEnabledExtensionNames = glfwExtensions; // NOTE: setting them up to be compatbile with older implementations
         createInfo.enabledLayerCount = 0;
+        createInfo.pNext = &layerSettingsCreateInfo;
 
         //VkResult result = vkCreateInstance(&createInfo, nullptr, &vulkInstance);
         // This is also possible on most calls for error checking and handling
@@ -78,6 +187,7 @@ private:
     void cleanup() {
         // Cleanup vulkan
         vkDestroyInstance(vulkInstance, nullptr);
+        vkDestroyDevice(device, nullptr);
 
         // Cleanup glfw
         glfwDestroyWindow(window);
